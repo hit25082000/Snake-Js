5dfed3e527b25172c8ffcb2d485d1af8d5df72c5
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2022-05-17 17:20:33 -0400

    XD

--- snake.js
+++ snake.js
@@ -4,13 +4,16 @@ var direcao = ""
 var comprimentoMatriz = 10
 var quantidadeComidas = 15
 var quantidadePedras = 10
+let xSnake = 5
+let ySnake = 5
+let snake = []
 var comida = () => { return Math.floor(Math.random() * 10); }
 var pedra = () => { return Math.floor(Math.random() * 10); }
+let table = document.createElement("table")
+areaMatriz()
 
 function mapearElementos() {
-    areaMatriz()
-    snake()
-    let table = document.createElement("table")
+    matriz[xSnake][ySnake] = `<img width="32px" src="icons8-snake-64.png">`
     table.style.textAlign = "center"
     table.style.width = "100%"
     table.style.height = "100%"
@@ -19,12 +22,11 @@ function mapearElementos() {
     body.append(table)
 
     for (let i = 0; i < quantidadeComidas; i++) {
-
         let xComida = comida()
         let yComida = comida()
 
         if (matriz[xComida][yComida] == undefined)
-            matriz[xComida][yComida] = `<img src="icons8-animal-de-rato-24.png">`
+            matriz[xComida][yComida] = `<img width="32px" src="icons8-animal-de-rato-24.png">`
         else
             i--
     }
@@ -39,6 +41,22 @@ function mapearElementos() {
         else
             i--
     }
+    atualizarCoordenadas()
+}
+
+function atualizarCoordenadas(direcao) {
+    snake.push([ySnake, xSnake])
+    console.log(snake)
+
+    if (matriz[ySnake][xSnake] == `<img width="32px" src="icons8-animal-de-rato-24.png">`)
+        snake[0].unshift(snake[0][0], snake[0][1])
+    else if (matriz[ySnake][xSnake] == `<img src="icons8-rocha-32.png">`)
+        matriz[snake[0][0]][snake[0][1]] = ``, snake.shift()
+    else
+        matriz[snake[0][0]][snake[0][1]] = ``
+
+    matriz[ySnake][xSnake] = `<img width="32px" src="icons8-snake-64.png">`
+    table.innerHTML = ""
 
     for (let x = 0; x < matriz.length; x++) {
         let tr = document.createElement("tr")
@@ -46,10 +64,14 @@ function mapearElementos() {
 
         for (let y = 0; y < matriz.length; y++) {
             let td = document.createElement("td")
+
             if (matriz[x][y] != undefined)
                 td.innerHTML = matriz[x][y]
+
             else if (matriz[x][y] == undefined)
-                td.innerHTML = "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;"
+                td.innerHTML = `&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;`
+
+            td.style.height = "50px"
             tr.appendChild(td)
         }
     }
@@ -63,20 +85,13 @@ function areaMatriz() {
 
 document.addEventListener("keydown", (event) => {
     if (event.key == "ArrowUp" && direcao != "baixo")
-        direcao = "cima", console.log(direcao)
+        direcao = "cima", ySnake--, atualizarCoordenadas(direcao), snake.shift()
     if (event.key == "ArrowDown" && direcao != "cima")
-        direcao = "baixo", console.log(direcao)
+        direcao = "baixo", ySnake++, atualizarCoordenadas(direcao), snake.shift()
     if (event.key == "ArrowLeft" && direcao != "direita")
-        direcao = "esquerda", console.log(direcao)
+        direcao = "esquerda", xSnake--, atualizarCoordenadas(direcao), snake.shift()
     if (event.key == "ArrowRight" && direcao != "esquerda")
-        direcao = "direita", console.log(direcao)
+        direcao = "direita", xSnake++, atualizarCoordenadas(direcao), snake.shift()
 })
-
-function snake() {
-    matriz[matriz.length / 2][matriz.length / 2] = `<img width="30px" src="icons8-snake-64.png">`
-    for (let i = 1; i <= matriz.length; i++) {
-
-    }
-}
 
 mapearElementos()
